6f6b7e9b38429486074a0525886745b6ba225161
Marcin Grzeskowiak <marcin.grzeskowiak@hotmail.com>
2014-11-09 20:00:53 +0100

    Refactoring

--- SmartPocket/ViewModels/ItemViewModel.cs
+++ SmartPocket/ViewModels/ItemViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using SmartPocket.Intefaces.Model;
 using System;
 using System.ComponentModel;
+using System.Data.Linq.Mapping;
 using System.Diagnostics;
 using System.Net;
 using System.Windows;
@@ -18,6 +19,7 @@ namespace SmartPocket.ViewModels
         /// Sample ViewModel property; this property is used to identify the object.
         /// </summary>
         /// <returns></returns>
+        
         public int ID
         {
             get
@@ -31,79 +33,6 @@ namespace SmartPocket.ViewModels
                     _id = value;
                     NotifyPropertyChanged("ID");
                 }
-            }
-        }
-
-        private string _lineOne;
-        /// <summary>
-        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
-        /// </summary>
-        /// <returns></returns>
-        public string LineOne
-        {
-            get
-            {
-                return _lineOne;
-            }
-            set
-            {
-                if (value != _lineOne)
-                {
-                    _lineOne = value;
-                    NotifyPropertyChanged("LineOne");
-                }
-            }
-        }
-
-        private string _lineTwo;
-        /// <summary>
-        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
-        /// </summary>
-        /// <returns></returns>
-        public string LineTwo
-        {
-            get
-            {
-                return _lineTwo;
-            }
-            set
-            {
-                if (value != _lineTwo)
-                {
-                    _lineTwo = value;
-                    NotifyPropertyChanged("LineTwo");
-                }
-            }
-        }
-
-        private string _lineThree;
-        /// <summary>
-        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
-        /// </summary>
-        /// <returns></returns>
-        public string LineThree
-        {
-            get
-            {
-                return _lineThree;
-            }
-            set
-            {
-                if (value != _lineThree)
-                {
-                    _lineThree = value;
-                    NotifyPropertyChanged("LineThree");
-                }
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void NotifyPropertyChanged(String propertyName)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (null != handler)
-            {
-                handler(this, new PropertyChangedEventArgs(propertyName));
             }
         }
 
@@ -210,6 +139,16 @@ namespace SmartPocket.ViewModels
                     _tags = value;
                     NotifyPropertyChanged("Tags");
                 }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
             }
         }
     }
